a6411df32523e0f7cc9b04a6c0a89c6c94253b92
SintoGenroku <borozda.a.s@gmail.com>
2022-10-17 22:11:46 +0300

    add custom exceptions

---
+++ Authentication.Common/Exceptions/ForbiddenException.cs
@@ -0,0 +1,10 @@
+﻿namespace Authentication.Common.Exceptions;
+
+public class ForbiddenException : Exception
+{
+    public ForbiddenException() {}
+        
+    public ForbiddenException(string message) : base(message) {}
+        
+    public ForbiddenException(string message, Exception inner) {}
+}
---
+++ Authentication.Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Authentication.Common.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException() {}
+        
+    public NotFoundException(string message) : base(message) {}
+        
+    public NotFoundException(string message, Exception inner) : base(message, inner) {}
+}

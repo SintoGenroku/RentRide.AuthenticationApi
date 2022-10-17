a0ee2ef487c58d7b9b91e4fd036da855ff6896dd
SintoGenroku <borozda.a.s@gmail.com>
2022-10-17 22:58:52 +0300

    add controllers

---
+++ Authentication.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,60 @@
+﻿using Authentication.Api.Models.Requests.Users;
+using Authentication.Api.Models.Responses.Errors;
+using Authentication.Common;
+using Authentication.Common.Exceptions;
+using Authentication.Services.Abstracts;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AuthenticationController : Controller
+{
+    private readonly IAuthenticationService _authenticationService;
+    private readonly IMapper _mapper;
+    
+    public AuthenticationController(IAuthenticationService authenticationService, IMapper mapper)
+    {
+        _authenticationService = authenticationService;
+        _mapper = mapper;
+    }
+
+    /// <summary>
+    ///     Registration endpoint
+    /// </summary>
+    /// <param name="userModel">Create user model</param>
+    /// <exception cref="BadRequestException"></exception>
+    [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status200OK)]
+    [HttpPost("registration")]
+    public async Task<IActionResult> RegisterAsync(UserRegistrationRequestModel userModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            var errors = ModelState.ToDictionary(
+                error => error.Key,
+                error => error.Value?.Errors.Select(e => e.ErrorMessage).ToArray()
+            );
+            
+            throw new BadRequestException("Invalid data", errors);
+        }
+
+        var user = _mapper.Map<User>(userModel);
+
+        var result = await _authenticationService.RegisterAsync(user, userModel.Password);
+
+        if (!result.IsSuccessfull)
+        {
+            var error = new Dictionary<string, string[]>
+            {
+                { "Error", result.ErrorMessages.ToArray() }
+            };
+            
+            throw new BadRequestException("Registration error", error);
+        }
+
+        return Ok();
+    }
+}

3a41433b69f8e33263ead1980c1b047bc8dc8f0e
SintoGenroku <borozda.a.s@gmail.com>
2022-10-17 22:21:55 +0300

    add db context

---
+++ Authentication.Data/AuthenticationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Authentication.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Data;
+
+public class AuthenticationDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    
+    public DbSet<Role> Roles { get; set; }
+
+    public AuthenticationDbContext() : base() { }
+
+    public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) 
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(user =>
+        {
+            user.HasMany(u => u.Roles)
+                .WithMany(r => r.Users);
+
+            user.Property(u => u.Fullname).IsRequired();
+            user.Property(u => u.Username).IsRequired();
+            user.Property(u => u.PasswordHash).IsRequired();
+            user.Property(u => u.MailAddress).IsRequired();
+            user.Property(u => u.PhoneNumber).IsRequired();
+        });
+
+        modelBuilder.Entity<Role>(role =>
+        {
+            role.Property(r => r.Name).IsRequired();
+        });
+    }
+}

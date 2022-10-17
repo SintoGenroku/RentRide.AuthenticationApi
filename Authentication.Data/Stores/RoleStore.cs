6afe8cdd6fcea86f218f7729359223ec628195da
SintoGenroku <borozda.a.s@gmail.com>
2022-10-17 22:27:11 +0300

    add custom stores

---
+++ Authentication.Data/Stores/RoleStore.cs
@@ -0,0 +1,121 @@
+﻿using Authentication.Common;
+using Authentication.Data.Abstracts;
+using Microsoft.AspNetCore.Identity;
+
+namespace Authentication.Data.Stores;
+
+public class RoleStore : IRoleStore<Role>
+{
+   private readonly IAuthenticationUnitOfWork _unitOfWork;
+
+
+    public RoleStore(IAuthenticationUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+
+    public void Dispose()
+    {
+    }
+
+    public async Task<IdentityResult> CreateAsync(Role role, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        await _unitOfWork.Roles.CreateAsync(role);
+
+        return IdentityResult.Success;
+    }
+
+    public async Task<IdentityResult> UpdateAsync(Role role, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (role == null)
+        {
+            throw new ArgumentNullException(nameof(role));
+        }
+
+        await _unitOfWork.Roles.UpdateAsync(role);
+
+        return IdentityResult.Success;
+    }
+
+    public Task<IdentityResult> DeleteAsync(Role role, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetRoleIdAsync(Role role, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (role == null)
+        {
+            throw new ArgumentNullException(nameof(role));
+        }
+
+        return Task.FromResult(role.Id.ToString());
+    }
+
+    public Task<string> GetRoleNameAsync(Role role, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (role == null)
+        {
+            throw new ArgumentNullException(nameof(role));
+        }
+
+        return Task.FromResult(role.Name);
+    }
+
+    public Task SetRoleNameAsync(Role role, string roleName, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (role == null)
+        {
+            throw new ArgumentNullException(nameof(role));
+        }
+
+        role.Name = roleName;
+
+        return Task.CompletedTask;
+    }
+
+    public Task<string> GetNormalizedRoleNameAsync(Role role, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (role == null)
+        {
+            throw new ArgumentNullException(nameof(role));
+        }
+
+        return Task.FromResult(role.Name.ToUpper());
+    }
+
+    public Task SetNormalizedRoleNameAsync(Role role, string normalizedName,
+        CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task<Role> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        var role = await _unitOfWork.Roles.GetByIdAsync(Guid.Parse(roleId));
+
+        return role;
+    }
+
+    public async Task<Role> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        var role = await _unitOfWork.Roles.GetByNameAsync(normalizedRoleName);
+
+        return role;
+    }
+}
---
+++ Authentication.Data/Stores/UserStore.cs
@@ -0,0 +1,208 @@
+﻿using Authentication.Common;
+using Authentication.Data.Abstracts;
+using Microsoft.AspNetCore.Identity;
+
+namespace Authentication.Data.Stores;
+
+public class UserStore : IUserPasswordStore<User>, IUserRoleStore<User>
+{
+    private readonly IAuthenticationUnitOfWork _unitOfWork;
+
+    public UserStore(IAuthenticationUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public void Dispose()
+    { }
+
+     public Task<string> GetUserIdAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            return Task.FromResult(user.Id.ToString());
+        }
+
+        public Task<string> GetUserNameAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            return Task.FromResult(user.Username);
+        }
+
+        public Task SetUserNameAsync(User user, string userName, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            return Task.FromResult(user.Username.ToUpper());
+        }
+
+        public Task SetNormalizedUserNameAsync(User user, string normalizedName, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task<IdentityResult> CreateAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            await _unitOfWork.Users.CreateAsync(user);
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> UpdateAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            await _unitOfWork.Users.UpdateAsync(user);
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> DeleteAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            await _unitOfWork.Users.DeleteAsync(user);
+            return IdentityResult.Success;
+        }
+
+        public async Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var user = await _unitOfWork.Users.GetByIdAsync(Guid.Parse(userId));
+            return user;
+        }
+
+        public async Task<User> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var user = await _unitOfWork.Users.GetByNameAsync(normalizedUserName);
+            return user;
+        }
+
+        public Task SetPasswordHashAsync(User user, string passwordHash, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            user.PasswordHash = passwordHash;
+            return Task.CompletedTask;
+
+        }
+
+        public Task<string> GetPasswordHashAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            return Task.FromResult(!string.IsNullOrEmpty(user.PasswordHash));
+        }
+
+        public async Task AddToRoleAsync(User user, string roleName, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var role = await _unitOfWork.Roles.GetByNameAsync(roleName);
+            user.Roles.Add(role);
+            await UpdateAsync(user, cancellationToken);
+        }
+
+        public async Task RemoveFromRoleAsync(User user, string roleName, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var role = await _unitOfWork.Roles.GetByNameAsync(roleName);
+            user.Roles.Remove(role);
+            await UpdateAsync(user, cancellationToken);
+        }
+
+        public Task<IList<string>> GetRolesAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null)
+            {
+                throw new ArgumentNullException();
+            }
+            var roles = user.Roles.Select(role => role.Name).ToList();
+            return Task.FromResult<IList<string>>(roles);
+        }
+
+        public Task<bool> IsInRoleAsync(User user, string roleName, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var isInRole = user.Roles.Any(role => role.Name == roleName);
+            return Task.FromResult(isInRole);
+        }
+
+        public Task<IList<User>> GetUsersInRoleAsync(string roleName, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+}

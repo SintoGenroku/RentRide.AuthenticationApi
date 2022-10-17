888c4e7abfcf72c58333c52f70a4a33104777ef6
SintoGenroku <borozda.a.s@gmail.com>
2022-10-17 22:54:58 +0300

    add mapper configurations

---
+++ Authentication.Api/MapperConfigs/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using Authentication.Api.Models.Requests.Users;
+using Authentication.Api.Models.Responses.Users;
+using Authentication.Common;
+using AutoMapper;
+
+namespace Authentication.Api.MapperConfigs;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<UserRegistrationRequestModel, User>()
+            .ForMember(request => request.Username, o => o.MapFrom(user => user.Login));
+        CreateMap<UserLoginRequestModel, User>();
+        CreateMap<UserEditRequestModel, User>();
+        CreateMap<User, UserResponseModel>();
+    }
+}

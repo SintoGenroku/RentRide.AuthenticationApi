e878f05bb6270d5eefde52543ebc18f29b756def
SintoGenroku <borozda.a.s@gmail.com>
2022-11-02 19:36:33 +0300

    add custom password validation

---
+++ Authentication.Api/Validators/CustomResourceOwnerPasswordValidator.cs
@@ -0,0 +1,39 @@
+﻿using Authentication.Services.Abstracts;
+using IdentityServer4.Models;
+using IdentityServer4.Validation;
+
+namespace Authentication.Api.Validators;
+
+public class CustomResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+{
+    private readonly IAuthenticationService _authenticationService;
+    private readonly IUserService _userService;
+
+    public CustomResourceOwnerPasswordValidator( IUserService userService, IAuthenticationService authenticationService)
+    {
+        _userService = userService;
+        _authenticationService = authenticationService;
+    }
+    public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+    {
+        var user = await _userService.GetByNameAsync(context.UserName);
+        if (user != null)
+        {
+            
+            var result = await _authenticationService.SignInAsync(user, context.Password);
+            // https://docs.identityserver.io/en/latest/reference/grant_validation_result.html#refgrantvalidationresult
+            if (result.IsSuccessfull)
+            {
+                context.Result = new GrantValidationResult(subject: user.Id.ToString(), authenticationMethod: "password");
+            }
+            else
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "invalid password");
+            }
+        }
+        else
+        {
+            context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "invalid login");
+        }
+    }
+}
---
+++ Authentication.Api/Validators/PasswordValidator.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+using Authentication.Common;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+
+namespace Authentication.Api.Validators;
+
+public class PasswordValidator : IPasswordValidator<User>
+{
+    public Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user, string password)
+    {
+        var regexes = new Dictionary<Regex, string>()
+        {
+            {new Regex(@"\d"), "Add digits(minimum 1)"},
+            {new Regex(@"[A-Z]"), "Add uppercase"},
+            {new Regex(@"[a-z]"), "Add lowercase"},
+            { new Regex(@"^[a-zA-Z0-9!@#$%^&*()_+|]+$"), "Such characters are not allowed." },
+        };
+        
+        foreach (var regex in regexes)
+        {
+            if (!regex.Key.IsMatch(password))
+            {
+                var error = new IdentityError()
+                {
+                    Description = regex.Value
+                };
+
+                var result = IdentityResult.Failed(error);
+
+                return Task.FromResult(result);
+            }
+        }
+
+        return Task.FromResult(IdentityResult.Success);
+    }
+}
